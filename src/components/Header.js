1b18b4da06800a3b37792ec346604f383acc695f
gksubed <20123043s@gmail.com>
2026-06-09 08:50:11 +0800

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+// components/Header.js
+
+import React from "react";
+
+function Header() {
+  return (
+    <header id="header">
+      <div id="top-bar">
+        <div className="container">
+          <ul id="top-buttons">
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+            <li className="divider"></li>
+                <li>
+                  <div className="language-switcher">
+                    <span><i className ="fa fa-globe"></i> English</span>
+                    <ul>
+                      <li><a href="#">Deutsch</a></li>
+                      <li><a href="#">Espa&ntilde;ol</a></li>
+                      <li><a href="#">Fran&ccedil;ais</a></li>
+                      <li><a href="#">Portugu&ecirc;s</a></li>
+                    </ul>
+                  </div>
+                </li>
+          </ul>
+        </div>
+      </div>
+      <div id="nav-section">
+        <div className="container">
+
+          <a href="/" className="nav-logo">
+            <img src={logo} alt="One Ring Rentals" />
+          </a>
+          <div id="sb-search" className="sb-search">
+                  <form>
+                    <input
+                      className="sb-search-input"
+                      placeholder="Search..."
+                      type="text"
+                   
+                      name="search"
+                      id="search"
+                    />
+                    <input className="sb-search-submit" type="submit" value="" />
+                    <i className="fa fa-search sb-icon-search"></i>
+                  </form>
+                </div>
+          <nav className="navbar">
+            <ul className="nav navbar-nav">
+              <li><a href="/">Find a Rental</a></li>
+              <li><a href="/articles">Recent Articles</a></li>
+              <li><a href="/regions">Regions</a></li>
+              <li><a href="/news">News</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
