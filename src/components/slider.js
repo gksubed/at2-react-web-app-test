04855d94db88497af9e2300212da14cf9c4d7c5f
gksubed <20123043s@gmail.com>
2026-06-09 09:44:19 +0800

    Add slider component

---
+++ src/components/slider.js
@@ -0,0 +1,78 @@
+<!-- BEGIN HOME SLIDER SECTION -->
+      <div
+        id="carousel-example-generic"
+        class="carousel slide"
+        data-ride="carousel"
+      >
+        <!-- Indicators 
+		  <ol class="carousel-indicators">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		  </ol> -->
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div
+            class="item active"
+            id="slide1"
+            style="
+              background: url(http://placehold.it/1920x605) no-repeat left
+                center;
+              background-size: cover;
+            "
+          >
+            <!-- Ready for JS Injection -->
+            <div class="carousel-caption">
+              <div class="caption sfr slider-title">Breathtaking views</div>
+              <div class="caption sfl slider-subtitle">
+                Relaxation in the Bay of Belfalas
+              </div>
+              <a href="#" class="caption sfb btn btn-default btn-lg"
+                >Learn More</a
+              >
+            </div>
+          </div>
+          <div
+            class="item"
+            id="slide2"
+            style="
+              background: url(http://placehold.it/1920x605) no-repeat left
+                center;
+              background-size: cover;
+            "
+          >
+            <div class="carousel-caption">
+              <div class="caption sfr slider-title">The simple life</div>
+              <div class="caption sfl slider-subtitle">
+                Lush gardens in Mordor
+              </div>
+              <a href="#" class="caption sfb btn btn-default btn-lg"
+                >Learn More</a
+              >
+            </div>
+          </div>
+        </div>
+        <!-- Blue Filter -->
+        <div id="home-search-section"></div>
+
+        <!-- Controls -->
+        <a
+          class="left carousel-control"
+          href="#carousel-example-generic"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="right carousel-control"
+          href="#carousel-example-generic"
+          role="button"
+          data-slide="next"
+        >
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <!-- END HOME SLIDER SECTION -->
