b7535c3bc355188dc16179430c944dc7ea59353d
gksubed <20123043s@gmail.com>
2026-06-09 10:36:41 +0800

    modified: App js with all component, function and router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ContactPage from "./pages/ContactPage";
+import NewsPage from "./pages/NewsPage";
+import RegionsPage from "./pages/RegionsPage";
+import ArticlePage from "./pages/ArticlePage";
+import Article from "./templates/Article";
 import './App.css';
 
 function App() {
@@ -9,7 +16,16 @@ function App() {
       <Header />
       <main className="app-content">
         {/* components go here */}
-        <p>Edit <code>src/app.js</code> to start building.</p>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/news" element={<NewsPage />} />
+            <Route path="/regions" element={<RegionsPage />} />
+            <Route path="/articles" element={<ArticlePage />} />
+            <Route path="/article/:id" element={<Article />} />
+          </Routes>
+        </BrowserRouter>
       </main>
       <Footer />
     </div>
