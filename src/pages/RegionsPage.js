a1487e2bbd1c3a5fe0296c343d6a69efccb48a90
gksubed <20123043s@gmail.com>
2026-06-09 10:28:32 +0800

    modified: regions page

--- src/pages/RegionsPage.js
+++ src/pages/RegionsPage.js
@@ -1,19 +1,35 @@
 // src/pages/RegionsPage.jsx
 
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Regions from "../components/Regions";    
+import { regions } from "../data/regions";
 
-function RegionsPage() {
-    return (
-        <div className="App">
-            <Header />
-            <main className="app-content">
-                {/* components go here */}
-                <Regions />
-            </main>
-            <Footer />
-        </div>
-    );
+function Regions() {
+  return (
+    <div className="container">
+    <div className="row">
+
+        <div className="grid-style1 clearfix">
+            {regions.map((region, index) => (
+                <div className="item col-md-4" key={index}>
+                <div className="image">
+                    <a href="#">
+                    <h3>{region.city}</h3>
+                    <span className="location">{region.state}</span>
+                    </a>
+
+                    <img src={region.image} alt={region.city} />
+
+                    <div className="region-description">
+                    <p>{region.description}</p>
+                    </div>
+                </div>
+                </div>
+            ))}
+            </div>
+      </div>
+    </div>
+  
+  );
 }
+
+export default Regions;
