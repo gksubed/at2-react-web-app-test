ce7be2b4f8890ceeb91cefcfdad8114ceeb38445
gksubed <20123043s@gmail.com>
2026-06-09 10:03:39 +0800

    Modify home.js

--- src/components/slider.js
+++ src/components/slider.js
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/css/style.css";
 import slide1 from "../assets/images/patterns/pattern8.jpg";
 import slide2 from "../assets/images/AjaxLoader.jpg";
+
+
 const slides = [
   {
     id: 1,
     image: slide1,
-     title: "Breathtaking views",
+    title: "Breathtaking views",
     subtitle: "Relaxation in the Bay of Belfalas",
     link: "/regions",
   },
@@ -20,6 +22,37 @@ const slides = [
   },
 ];
 
+function Slider() {
+  const [current, setCurrent] = useState(0);
+
+  // Auto Slide
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrent((prev) =>
+        prev === slides.length - 1 ? 0 : prev + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  // Previous Slide
+  const prevSlide = (e) => {
+    e.preventDefault();
+
+    setCurrent((prev) =>
+      prev === 0 ? slides.length - 1 : prev - 1
+    );
+  };
+
+  // Next Slide
+  const nextSlide = (e) => {
+    e.preventDefault();
+
+    setCurrent((prev) =>
+      prev === slides.length - 1 ? 0 : prev + 1
+    );
+  };
 
   return (
     <>
@@ -27,85 +60,86 @@ const slides = [
       <div
         id="carousel-example-generic"
         className="carousel slide"
-      ></div>
-      <div
-        id="carousel-example-generic"
-        class="carousel slide"
-        data-ride="carousel"
       >
-        <!-- Indicators 
-		  <ol class="carousel-indicators">
-		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		  </ol> -->
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div
-            class="item active"
-            id="slide1"
-            style="
-              background: url(http://placehold.it/1920x605) no-repeat left
-                center;
-              background-size: cover;
-            "
-          >
-            <!-- Ready for JS Injection -->
-            <div class="carousel-caption">
-              <div class="caption sfr slider-title">Breathtaking views</div>
-              <div class="caption sfl slider-subtitle">
-                Relaxation in the Bay of Belfalas
+        {/* Wrapper for slides */}
+        <div
+          className="carousel-inner"
+          role="listbox"
+        >
+
+          {slides.map((slide, index) => (
+            <div
+              key={slide.id}
+              id={slide.id}
+              className={`item ${
+                index === current ? "active" : ""
+              }`}
+              style={{
+                background: `url(${slide.image}) no-repeat left center`,
+                backgroundSize: "cover",
+                height: "605px",
+                display:
+                  index === current ? "block" : "none",
+                position: "relative",
+              }}
+            >
+
+              {/* Ready for JS Injection */}
+              <div className="carousel-caption">
+
+                <div className="caption sfr slider-title">
+                  {slide.title}
+                </div>
+
+                <div className="caption sfl slider-subtitle">
+                  {slide.subtitle}
+                </div>
+
+                <a
+                  href={slide.link}
+                  className="caption sfb btn btn-default btn-lg"
+                >
+                  Learn More
+                </a>
+
               </div>
-              <a href="#" class="caption sfb btn btn-default btn-lg"
-                >Learn More</a
-              >
             </div>
-          </div>
-          <div
-            class="item"
-            id="slide2"
-            style="
-              background: url(http://placehold.it/1920x605) no-repeat left
-                center;
-              background-size: cover;
-            "
-          >
-            <div class="carousel-caption">
-              <div class="caption sfr slider-title">The simple life</div>
-              <div class="caption sfl slider-subtitle">
-                Lush gardens in Mordor
-              </div>
-              <a href="#" class="caption sfb btn btn-default btn-lg"
-                >Learn More</a
-              >
-            </div>
-          </div>
+          ))}
         </div>
-        <!-- Blue Filter -->
-        <div id="home-search-section"></div>
 
-        <!-- Controls -->
+        {/* Blue Filter */}
+        {/* <div id="home-search-section"></div> */}
+
+        {/* Controls */}
         <a
-          class="left carousel-control"
+          className="left carousel-control"
           href="#carousel-example-generic"
           role="button"
-          data-slide="prev"
+          onClick={prevSlide}
         >
-          <span class="glyphicon glyphicon-chevron-left"></span>
-          <span class="sr-only">Previous</span>
+          <span className="glyphicon glyphicon-chevron-left"></span>
+
+          <span className="sr-only">
+            Previous
+          </span>
         </a>
+
         <a
-          class="right carousel-control"
+          className="right carousel-control"
           href="#carousel-example-generic"
           role="button"
-          data-slide="next"
+          onClick={nextSlide}
         >
-          <span class="glyphicon glyphicon-chevron-right"></span>
-          <span class="sr-only">Next</span>
+          <span className="glyphicon glyphicon-chevron-right"></span>
+
+          <span className="sr-only">
+            Next
+          </span>
         </a>
+
       </div>
-      <!-- END HOME SLIDER SECTION -->
-         {/* END HOME SLIDER SECTION */}
+      {/* END HOME SLIDER SECTION */}
     </>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Slider from "../components/Slider";
 
 
 function Home() {
   return (
     <div>
-      
+      <Slider />
 
     </div>
   );
